ca2ff900101a06dca9aa1fedb62f957a56169047
krishpranav <krisna.pranav@gmail.com>
2021-09-11 17:45:48 +0530

    pingc: consoleio header

--- pingc/consoleio.h
+++ pingc/consoleio.h
@@ -4,22 +4,29 @@
  * SPDX-License-Identifier: MIT License
 */
 
+// imports
 #import <Foundation/Foundation.h>
+#import "tcping.h"
 
 /**
-@brief: enum value
-*/
+ @brief: enum
+ */
 
 enum OutputType: NSInteger {
     OutputTypeError,
     OutputTypeStandard
 };
 
+NS_ASSUME_NONNULL_BEGIN
+
 /**
-@brief: interface ConsoleIO
-*/
+ @brief: ConsoleIO interface class
+ */
 
 @interface ConsoleIO : NSObject
-+ (void) writeMessage: (NSString *)message to:(NSInteger) to;
-
++ (void)writeMessage:(NSString *)message to:(NSInteger) to;
++ (void)printUsage;
++ (void)printReulst:(BOOL)isFinish detail:(Tcping *)detail count:(NSUInteger)count lossCount:(NSUInteger)lossCount min:(NSNumber *)min max:(NSNumber *)max avge:(NSNumber *)avge;
 @end
+
+NS_ASSUME_NONNULL_END
