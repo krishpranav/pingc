2aa1f6a507a644578d8e934f013d33355936ef8d
krishpranav <krisna.pranav@gmail.com>
2021-09-11 17:44:45 +0530

    pingc: tcping
    update header

--- pingc/tcping.h
+++ pingc/tcping.h
@@ -3,14 +3,18 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Tcping : NSObject
-
 @property (nonatomic, strong) NSDate * startTime;
+@property (nonatomic) NSTimeInterval speed;
+@property (nonatomic, copy) NSString * domain;
+@property (nonatomic, copy) NSString * host;
+@property (nonatomic) UInt16 port;
+@property (nonatomic) dispatch_group_t group;
+@property (nonatomic) dispatch_queue_t queue;
 
-- (instancetype)initWith:(dispatch_group_t)group queue:(dispatch_queue_t)queue;
+- (instancetype)initWith:(dispatch_group_t)group queue:(dispatch_queue_t) queue;
 
 - (void)connectSocket:(NSString *)domain port:(UInt16)port;
 
 @end
-
 
 NS_ASSUME_NONNULL_END
